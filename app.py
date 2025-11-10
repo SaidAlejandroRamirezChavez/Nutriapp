521efde6d69ec67d5c66ba36169be2c3cb202343
SaidAlejandroRamirezChavez <24308060610069@cetis61.edu.mx>
2025-11-09 23:03:41 -0700

    pantalla de registro semifuncinal

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import  Flask, render_template
-import tkinter as tk
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("Registro.html")
+
+@app.route("/verificacion", methods=["GET", "POST"])
+def verificacion():
+    # Leer correctamente los valores enviados por el formulario
+    contraseña = request.form.get("contraseña")
+    confirmar = request.form.get("confirmar")
+
+    if contraseña != confirmar:
+        error_message = "Las contraseñas no coinciden. Por favor, inténtalo de nuevo."
+        return render_template("Registro.html", error=error_message)
+    else:
+        # Redirigir al endpoint /inicio después de un registro/verificación exitosa
+        return redirect(url_for('inicio'))
+
+
 
 @app.route("/inicio")
 def inicio():
